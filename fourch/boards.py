27e0ed96bdfb16e00f4b17e59684585b62e3ad7b
Chris Hansen <chris@gibsonsec.org>
2014-06-03 17:41:34 +1000

    Merge in feblehober123/4ch (patch-1 and update-1) and fix whitespace.

--- fourch/boards.py
+++ fourch/boards.py
@@ -9,45 +9,45 @@ class listed_board:
         It is used by fourch.boards(), a function to
         get a list of all the boards and their properties.
     """
-    
+
     def __init__(self, json):
         """ Initialize the board with the information it has.
-            
+
             :param json: the json data for this post
             :type json: dict
         """
         self._json = json
-    
+
     @property
     def board(self):
         """The board name, minus slashes. e.g., 'b', 'x', 'tv'"""
         return self._json['board']
-    
+
     @property
     def title(self):
         """The title of the board, e.g., 'Random', 'Paranormal', 'Television & Film'"""
         return self._json['title']
-    
+
     @property
     def ws(self):
         """Is the board worksafe?"""
         return bool(self._json['ws_board'])
-    
+
     @property
     def pages(self):
         """How many pages does the board have?"""
         return self._json['pages']
-    
+
     @property
     def per_page(self):
         """How many posts are there on 1 page?"""
         return self._json['per_page']
-    
+
 
 def boards(https=False):
     """ Get a list of all boards on 4chan, in :class:`fourch.boards.listed_board` objects
         containing information about the boards.
-        
+
         :param https: Should we use HTTPS or HTTP?
         :type https: bool
     """
@@ -58,7 +58,7 @@ def boards(https=False):
     r = s.get(url)
     if r.status_code != requests.codes.ok:
         r.raise_for_status()
-    
+
     boards = []
     for json_board in r.json()['boards']:
         boards.append(listed_board(json_board))
